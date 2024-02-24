28d91dfacc9caa1440202668850cc87c57a16a6c
Shunsuke Sakairi <sakairi.shunsuke@gmail.com>
2024-02-25 01:36:23 +0900

    今日終わり

--- app/components/layouts/Header/index.tsx
+++ app/components/layouts/Header/index.tsx
@@ -10,29 +10,31 @@ export const Header: React.FC = () => {
   const router = useRouter();
 
   return (
-    <header className={styles.container}>
-      <h1 onClick={() => router.push("/")}>
-        <Image
-          src={Logo}
-          width={150}
-          height={60}
-          alt="Lamina Logo"
-          priority
-        ></Image>
-      </h1>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/Works">Works</Link>
-          </li>
-          <li>
-            <Link href="/Profile">Profile</Link>
-          </li>
-        </ul>
-      </nav>
+    <header className={styles.headerArea}>
+      <div className={styles.headerContainer}>
+        <h1 onClick={() => router.push("/")}>
+          <Image
+            src={Logo}
+            width={150}
+            height={60}
+            alt="Lamina Logo"
+            priority
+          ></Image>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/Works">Works</Link>
+            </li>
+            <li>
+              <Link href="/Profile">Profile</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
