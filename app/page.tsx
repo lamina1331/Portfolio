350e5ae9ebbecedc942606cdae3315a256833b1c
Shunsuke Sakairi <sakairi.shunsuke@gmail.com>
2024-02-25 11:46:49 +0900

    edit hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.scss";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Homeページ</h1>
-    </div>
+    <main className={styles.main}>
+      <section className={styles.hero}>
+        <h1>Lamina Portfolio</h1>
+        <p>Creative and Technology</p>
+        <Link href="#Works">View Works</Link>
+      </section>
+      <section className={styles.about}>
+        <h2>Homeページのコンテンツ</h2>
+        {/* ここにsecを追加していく News Information About me Works（Preview） Contactなど*/}
+        <section id="Works">
+          <div>test</div>
+        </section>
+      </section>
+    </main>
   );
 }
