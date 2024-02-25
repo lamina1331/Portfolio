62b5122fd18b5329ff2ff14c84da8f07cc0e6108
Shunsuke Sakairi <sakairi.shunsuke@gmail.com>
2024-02-25 23:29:17 +0900

    microcms setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-"use client";
-
 import Image from "next/image";
 import styles from "./page.module.scss";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home(work: any) {
   return (
     <main className={styles.main}>
       <section className={styles.hero}>
@@ -13,11 +11,9 @@ export default function Home() {
         <Link href="#Works">View Works</Link>
       </section>
       <section className={styles.about}>
-        <h2>Homeページのコンテンツ</h2>
+        <h2>Works</h2>
         {/* ここにsecを追加していく News Information About me Works（Preview） Contactなど*/}
-        <section id="Works">
-          <div>test</div>
-        </section>
+        <section id="Works"></section>
       </section>
     </main>
   );
